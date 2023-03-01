4c0b1242a9c6bd9faf90f8accda311b8fb2f3da3
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-03-01 09:53:40 +0530

    Combine related requests into a single transaction

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.db import transaction
 from django.templatetags.static import static
 
 from rest_framework.decorators import api_view
@@ -86,6 +87,7 @@ class OrderSerializer(ModelSerializer):
 
 
 @api_view(['POST'])
+@transaction.atomic
 def register_order(request):
     serializer = OrderSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)

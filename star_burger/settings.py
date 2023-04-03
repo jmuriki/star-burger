70cbef8b7e442b215f008c03ad36889d212ae0b0
jmuriki <106560016+jmuriki@users.noreply.github.com>
2023-04-03 10:02:01 +0530

    Switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -94,9 +94,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'starburger_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [

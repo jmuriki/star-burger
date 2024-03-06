bd958b6cbb23c0d702398e7e00ed7a2f4fda6c5c
jmuriki <jmuriki@mail.ru>
2024-03-06 20:30:38 +0530

    Create deploy_starburger_docker_compose.sh

---
+++ scripts/deploy_starburger_docker_compose.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+cd /opt/star-burger/
+
+# Остановка текущего стэка
+docker compose -f docker-compose_gunicorn.yaml down
+
+# Cкачивание свежих образов из .yaml файлов
+docker compose -f docker-compose_gunicorn.yaml pull
+
+# Запуск стэка с prod-версией:
+docker compose -f docker-compose_gunicorn.yaml up -d
+
+# Перезапуск nginx
+systemctl reload nginx.service
+
+# Загрузка переменных окружения из файла .env
+if [[ -f .env ]]; then
+    source .env
+fi
+
+# Установка переменной с токеном доступа Rollbar
+access_token=$ROLLBAR_ACCESS_TOKEN
+
+# Получение хэша последнего коммита
+commit_hash=$(docker inspect --format='{{index .Config.Labels "last_commit_hash"}}' jmuriki/star-burger_backend)
+
+# Отправка запроса на API Rollbar с информацией о деплое
+curl -X POST https://api.rollbar.com/api/1/deploy/ \
+     -H "Content-Type: application/json" \
+     -H "X-Rollbar-Access-Token: $access_token" \
+     -d '{
+           "environment": "production",
+           "revision": "'"$commit_hash"'",
+           "rollbar_name": "jmuriki",
+           "local_username": "jmuriki-root",
+           "comment": "Комментарий",
+           "status": "succeeded"
+         }'
+
+# Сообщение об успешном завершении деплоя
+echo "Деплой успешно завершен!"
